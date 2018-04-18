de52d2883494a841fe302811d6187615fc8c3da9
Taylor <Taylormoyle93@gmail.com>
2018-04-17 19:51:39 -0700

    Add User button and Windows :snake:

--- Main_CV2.py
+++ Main_CV2.py
@@ -1,7 +1,9 @@
 import cv2
 from tkinter import *
+import time
 import numpy as np
-import os
+from PIL import Image, ImageTk
+import Controller_CV2 as con
 
 RES = 300
 username = ""
@@ -34,8 +36,7 @@ def login():
         root.overrideredirect(1)
         root.bind('<Escape>', quit)
 
-        eye_file = os.path.join('pics', 'eye.gif')
-        bg_image = PhotoImage(file=eye_file)
+        bg_image = PhotoImage(file='pics\\eye.gif')
         w = bg_image.width()
         h = bg_image.height()
         bg_label = Label(root, image=bg_image)
@@ -70,8 +71,7 @@ def menu():
     root = Tk()
     root.bind('<Escape>', quit)
 
-    menu_pic = os.path.join('pics', 'menu.gif')
-    bg_image = PhotoImage(file=menu_pic)
+    bg_image = PhotoImage(file='pics\\menu.gif')
     w = bg_image.width()
     h = bg_image.height()
     bg_label = Label(root, image=bg_image)
@@ -82,19 +82,39 @@ def menu():
     def close_window():
         root.destroy()
 
+    def update(entry_name, toplevel):
+        name = entry_name.get()
+        list_users.insert(END, name)
+        entry_name.delete(0, last=len(name))
+        toplevel.destroy()
+        display_video(mode='add_user', name=name)
+
+    def add_user():
+        name_text = 'Enter the User\'s Name: '
+        toplevel = Toplevel()
+        lbl_name = Label(toplevel, text=name_text, height=2, width=20)
+        lbl_name.grid(column=0, row=0)
+        entry_name = Entry(toplevel, width=20)
+        entry_name.grid(column=1, row=0, padx=20)
+        entry_name.bind('<Return>', (lambda event: update(entry_name, toplevel)))
+
     scrollbar = Scrollbar(root)
-    scrollbar.grid(column=5, row=0, sticky=N + S, pady=10, rowspan=7)
+    scrollbar.grid(column=5, row=0, sticky=N+S, pady=10, rowspan=7)
     list_users = Listbox(root, yscrollcommand=scrollbar.set)
     list_users.grid(column=0, row=0, padx=10, pady=10, rowspan=7, columnspan=4)
-    list_users.config(width=35, height=26)
+    list_users.config(width=65, height=26)
     scrollbar.config(command=list_users.yview)
+
+    btn_add = Button(root, text='Add', width=12, command=lambda: add_user())
+    btn_add.configure(background='black', foreground='white')
+    btn_add.grid(column=6, row=0, padx=35, pady=10)
 
     root.protocol("WM_DELETE_WINDOW", close_window)
     root.mainloop()
 
 
 # capture video feed, frame by frame
-def display_video():
+def display_video(mode, name):
     cap = cv2.VideoCapture(0)
     success = True
     shrug = cv2.imread('pics\\shrug.png')
@@ -108,6 +128,14 @@ def display_video():
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
+        if cv2.waitKey(1) & 0xFF == ord('s'):
+            if mode == 'add_user':
+                cv2.imwrite('users\\' + name + '.png', resized_frame)
+                break
+            else:
+                cv2.imwrite('frames\\' + (time.time()*1000) + '.png', resized_frame)
+                break
 
         '''
          if np.argmax(prediction) == 0:
